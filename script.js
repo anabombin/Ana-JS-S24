68ca9c478ff2963456de1a78a388947e725a9445
anabombin <bomba227@newschool.edu>
2024-05-11 18:54:23 -0400

    compiled projects

--- script.js
+++ script.js
@@ -3,28 +3,28 @@ $( document ).ready(function() {
 
     // Close travel record info window
     $("#less1").click(function(){
-        $("#change").toggle();
+        $("#change").slideToggle();
         $("#gen-tool").css("display","none");
         $("#send").css("display","none");
         $("body").css("background-image","url('images/changing.png')");
     });
 
     $("#less2").click(function(){
-        $("#gen-tool").toggle();
+        $("#gen-tool").slideToggle();
         $("#change").css("display","none");
         $("#send").css("display","none");
         $("body").css("background-image","url('images/tool.png')");
     });
 
     $("#less3").click(function(){
-        $("#send").toggle();
+        $("#send").slideToggle();
         $("#gen-tool").css("display","none");
         $("#change").css("display","none");
         $("body").css("background-image","url('images/sender.png')");
     });
 
     $("#less4").click(function(){
-        $("#links").toggle();
+        $("#links").slideToggle();
     });
 
 });
